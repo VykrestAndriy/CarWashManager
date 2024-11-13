7dfb42128c388b225ac882f8f46233004d09a263
VykrestAndriy <andry20053004@gmail.com>
2024-11-13 18:58:02 +0200

    Add files via upload

---
+++ CarWashManager.BuisnessLogic/Contracts/IWashService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarWashManager.BusinessLogic.Dtos;
+
+namespace CarWashManager.BusinessLogic.Contracts;
+
+public interface IWashService
+{
+    Task<IReadOnlyList<WashDto>> Get();
+    Task<WashDto> Get(string washId);
+    Task Add(WashDto wash);
+    Task Update(WashDto wash);
+    Task Remove(string washId);
+}
---
+++ CarWashManager.BuisnessLogic/Factories/WashServiceOrderFactory.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CarWashManager.BuisnessLogic.Factories
+{
+    public abstract class WashServiceOrderFactory
+    {
+        public abstract IWashOrder CreateOrder(string orderId, string washType, decimal amount);
+    }
+}
