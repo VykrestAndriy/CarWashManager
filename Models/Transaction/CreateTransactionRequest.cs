ba4b919c6a692884a0301802681d5a281a11ec86
VykrestAndriy <andry20053004@gmail.com>
2024-11-26 22:57:48 +0200

    Add files via upload

---
+++ Models/Transaction/CreateTransactionRequest.cs
@@ -0,0 +1,17 @@
+﻿using CarWashManager.Core.Enums;
+using System.ComponentModel.DataAnnotations;
+using CarWashManager.Core.Entities;
+
+namespace CarWashManager.Models.Transactions;
+
+public class CreateTransactionRequest
+{
+    [Required]
+    public string WashId { get; init; }
+
+    [Required]
+    public TransactionType TransactionType { get; init; }
+
+    [Required]
+    public decimal Amount { get; init; }
+}
